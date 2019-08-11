a94764a774cb860919c4a9ea10f38f94d37a5937
Matteo 'Gand988' Zanda <gand988@gmail.com>
2019-08-11 18:07:01 +0200

    add score + moves, ending screen with total moves. Start working on the reset game

---
+++ README.md
@@ -0,0 +1,5 @@
+# Memory Card Game
+[Link](https://memory-card-game.netlify.com/)
+
+Memory Card Game made with html5/css/js for the [DevCord](https://discord.gg/devcord) challenge.
+
--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 let first = [];
+var addPoint = 0;
+document.querySelector('.score').innerHTML = `SCORE: ${addPoint}`;
+var addMoves = 0;
+document.querySelector('.moves').innerHTML = ` ${addMoves} :MOVES`;
 
 // fill the array with x2 same value, in the same array.
-for (let i = 1; i <= 26; i++) {
-// for (let i = 1; i <= 5; i++) {
-  first.push(i,i);
-  // console.log(first); 
+function populateArray(myArray){
+  for (let i = 1; i <= 26; i++) {
+    first.push(i,i);
+    // console.log(first); 
+    }
 }
-
+populateArray(first);
 // accedi random ad una posizione, copia il valore in un altro array e 
 
 
@@ -29,10 +34,15 @@ let firstDeck = document.querySelectorAll('.card');
 let play = []; // array momentaneo per confronto carte
 
 // populate the cards with the number and add the event for flip the card
-for(let i = 0; i < firstDeck.length; i++){
-  firstDeck[i].textContent = p[i];
-  firstDeck[i].addEventListener('click', flip);
+function giveEvent(){
+  for(let i = 0; i < p.length; i++){
+    firstDeck[i].style.opacity = '1';
+    firstDeck[i].textContent = p[i];
+    firstDeck[i].addEventListener('click', flip);
+  }
 }
+
+giveEvent();
 
 function flip(){
   // log too see if works 
@@ -40,33 +50,37 @@ function flip(){
   if (play.length != '2'){
     play.push(this.textContent);
     // console.log(play); 
-
-    // ! maybe i can remove this IF
-
-    // if (this.classList.contains('flip')) {
-    //   this.classList.remove("flip");
-    // } else {
-      // this.style.zIndex = '220';
-      this.classList.add("flip");
-      this.removeEventListener('click', flip);
-    // }
-    // ! check this one ()
+    // addMoves++;
+    this.classList.add("flip");
+    this.removeEventListener('click', flip);
     if ((play.length == '2') && (play[0] == play[1])){
       console.log('match');
+
+      addPoint++; 
+      document.querySelector('.score').innerHTML = `SCORE: ${addPoint}`;
+      // console.log(addPoint);
+      
       setTimeout(() => {
         document.getElementsByClassName('flip')[1].style.opacity = '0';
         document.getElementsByClassName('flip')[0].style.opacity = '0';
         clearNewMove();
       }, (1000));
+      addMoves++;
+
     } else if ((play.length == '2') && (play[0] != play[1])){
       console.log('not match');
       setTimeout(()=>{
         document.getElementsByClassName('flip')[1].addEventListener('click', flip);
         document.getElementsByClassName('flip')[0].addEventListener('click', flip);
-        // this.addEventListener('click', flip);
         clearNewMove();
       }, 1000); 
+      addMoves++;
     }
+    document.querySelector('.moves').innerHTML = `${addMoves} :MOVES`;
+  }
+    if( addPoint == (first.length / 2)){
+    document.querySelector('.congrat').style.display = 'flex';
+    document.querySelector('.final').textContent = `You finish the game in ${addMoves} moves`
   }
 };
 
@@ -75,6 +89,22 @@ var clearNewMove= ()=>{
   document.getElementsByClassName('flip')[0].classList.remove('flip');
   play = []; 
 };
-// function disable(){
-//   this.removeEventListener('click', flip);
-// }
+
+// document.querySelector('.reset').addEventListener('click', () => {
+//   p = shuffle(first);
+//   giveEvent();
+//   document.querySelector('.congrat').style.display = '';
+
+// });
+
+// force portrait
+
+// document.addEventListener("orientationchange", function (event) {
+//   switch (window.orientation) {
+//     case -90: case 90:
+//       /* Device is in landscape mode */
+//       break;
+//     default:
+//     /* Device is in portrait mode */
+//   }
+// });
